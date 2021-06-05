1af089213ac826c2e9aea90f30881d097090e738
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-06-05 15:56:46 -0400

    homework

---
+++ about.js
@@ -0,0 +1,138 @@
+
+import React, { Component } from 'react';
+import './about.css';
+
+
+// import {appendScript} from './random'
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                     <div>
+            <div id="parallax-world-of-ugg">
+  
+  <section>
+    <div className="title">
+      <h1>Sing the Queen City</h1>
+    </div>
+  </section>
+  
+  <section>
+      <div className="parallax-one">
+        <h2>Welcome Home</h2>
+        <h2>Love, Cincy</h2>
+      </div>
+  </section>
+  
+
+
+
+  <section>
+    <div className="block">
+      <p className="header1">A Gem of the Midwest</p>
+      <p className="text1">Cincinnati has so much to offer. We can't stop talking about her.</p>
+
+      
+    </div>
+  </section>
+  
+  <section>
+    <div className="parallax-two">
+      <h2>Explore the Seven Hills</h2>
+    </div>
+  </section>
+  <div className="block">
+      <p className="header2">Wanderlust Welcome Here</p>
+      <p className="">Explore fifty two neighborhoods in our expansive city limits. There is so much to see and experience. Each neighborhood is unique and special with its attractions, businesses, and histories; go on and find your new home away from home. </p>
+  <section>
+    
+  </section>
+  </div>
+
+  <section>
+    <div className="parallax-three">
+      <h2>On the Banks</h2>
+    </div>
+  </section>
+  <div className="block">
+    <p className="header3">Down to the River to Play</p>
+    <p>Cincinnati has beautiful riverfront parks, walkable paths, fun businesses and attractions, and amazing views on the banks of the Ohio River. Enjoy the cool breezes, open air, and some of the best sights Cincinnati has to offer.</p>
+  <section>
+     
+   </section>
+</div>
+
+
+   <section>
+      <div className="parallax-four">
+        <h2>Arts & Culture</h2>
+      </div>
+  </section>
+  <div className="block">
+    <p className="header3">Sophistication for the Refined Traveler</p>
+    <p>From Music Hall to our art museums, galleries, theaters, and playhouses, Cincinnat's vibrant arts' scene is alive and well.</p>
+    <section>
+  </section>
+    </div>
+
+
+
+
+   <section>
+      <div className="parallax-five">
+        <h2>Sports and Entertainment</h2>
+      </div>
+  </section>
+  <section>
+  <div className="block">
+    <p className="header3">Hey, Batter Batter</p>
+    <p>Home of the first professional baseball team, the Cincinnati Reds, as well as the Bengals professional football and FC Cincinnati soccer, in addition to our numerous college teams, Cincinnati is ideal for the sports lover.</p>
+  </div>
+ 
+  </section>
+ 
+
+
+    <section>
+      <div className="parallax-six">
+        <h2>Love the Outdoors</h2>
+      </div>
+  </section>
+  <section>
+  <div className="block">
+      <p className="header3">Unparalled Natural Beauty</p>
+      <p>From its verdant hills and valleys to shining river, Cincinnati is a natural wonder. The city boasts of numerous public parks with extensive grounds, hiking trails, as well as the largest interconnected biking trails in the United States. Go on, adventure is out there.</p>
+   </div>
+   </section>
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <section className="text"></section>
+  <section id="hero">
+    <div id="cincy1"></div>
+    <div id="cincy2"></div>
+  </section>
+  <section className="text"></section>
+
+</div>
+
+        
+        </div>
+            </div>
+        )
+    }
+}
+
---
+++ footer.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+
+
+class Footer extends React.Component
+{
+  render()
+  {
+    return (
+      <nav class="navbar navbar-expand-sm bg-white">
+      <div className="footer-home"></div>
+    <div className="footer-info"></div>
+    <div className="footer-capstone-info"></div>
+          <h3>Kable Academy Capstone Project</h3>
+        
+        <div className="footer-info"></div>
+<div class="container"></div>
+  <div class="row"></div>
+    <div class="col">
+    <h3>Domynique Shelby</h3></div>
+    <a href="" rel="noreferrer" target="_blank">Github</a>
+    <a href="" rel="noreferrer" target="_blank">LinkedIn</a>
+     
+   <div className="footer-info"></div>
+<div class="container"></div>
+  <div class="row"></div>
+    <div class="col"></div> 
+ <h3>Philicia Watson</h3>
+ <a href="" rel="noreferrer" target="_blank">Github</a>
+  <a href="" rel="noreferrer" target="_blank">LinkedIn</a>
+           
+    <div className="footer-info"></div>
+<div class="container"></div>
+  <div class="row"></div>
+    <div class="col"></div>
+    <h3>Allison Hall</h3>
+     <a href="" rel="noreferrer" target="_blank">Github</a>
+     <a href="" rel="noreferrer" target="_blank">LinkedIn</a>
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      
+    
+    </nav>
+
+
+    )
+  }
+}
+export default Footer;
+
+
